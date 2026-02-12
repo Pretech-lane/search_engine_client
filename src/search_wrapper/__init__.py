b61d286fe3259dd8eb370d0740a8771083d08fe0
Prince71-dev <chukwudi@initsng.com>
2026-02-12 09:56:32 +0100

    Updated the _init_

--- src/search_wrapper/__init__.py
+++ src/search_wrapper/__init__.py
@@ -2,4 +2,7 @@ from .engines.serper import GoogleSearchFromSerper
 from .engines.duckduckgo import DuckDuckGoSearch
 from .engines.bing import BingSearch
 
-__all__ = ["GoogleSearchFromSerper"]
+__all__ = ["GoogleSearchFromSerper",
+           "DuckDuckGoSearch",
+            "BingSearch"
+            ]
